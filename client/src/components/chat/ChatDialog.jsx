c9f586eb6b763644b1f220a973d790a7709cef47
Sachin Bankoti <sachinbankoti16@gmail.com>
2024-06-27 13:11:33 -0400

    Added header Menu and Search Icon

--- client/src/components/chat/ChatDialog.jsx
+++ client/src/components/chat/ChatDialog.jsx
@@ -1,8 +1,44 @@
+import { Box, Dialog, styled } from '@mui/material'
 import React from 'react'
+import Menu from './Menu/Menu';
+import EmptyChat from './chat/EmptyChat';
 
+const dialogStyle = {
+    height: "95%",
+    width: "100%",
+    margin: '20px',
+    borderRadius: 0,
+    maxWidth: "100%",
+    maxHeight: "100%",
+    boxShadow: "none",
+    overflow: "hidden",
+  };
+const Component = styled(Box)`
+  display: flex;
+`
+const LeftComponent = styled(Box)`
+  min-width: 450px;
+`
+const RightComponent = styled(Box)`
+  width: 73%;
+  min-width: 300px;
+  height: 100%;
+  border-left: 1px solid rgba(0, 0, 0, 0.14);
+`
 const ChatDialog = () => {
   return (
-    <div>ChatDialog</div>
+    <div>
+        <Dialog open={true} PaperProps={{ sx: dialogStyle }} hideBackdrop={true} maxWidth={'md'}>
+            <Component>
+                <LeftComponent>
+                    <Menu />
+                </LeftComponent>
+                <RightComponent>
+                    <EmptyChat />
+                </RightComponent>
+            </Component>
+        </Dialog>
+    </div>
   )
 }
 
---
+++ client/src/components/chat/Menu/HeaderMenu.jsx
@@ -0,0 +1,44 @@
+import { MoreVert } from "@mui/icons-material";
+import { Menu, MenuItem,Box,styled} from "@mui/material";
+import { useState } from "react";
+
+const MenuOption = styled(MenuItem)`
+    font-size: 14px;
+    padding: 15px 60px 5px 24px;
+    color: #4A4A4A;
+`
+const HeaderMenu = () => {
+    const [open,setOpen] = useState(null);
+    const handleClose =()=>{
+        setOpen(null);
+    }
+    const handleClick =(e)=>{
+        setOpen(e.currentTarget);
+    }
+  return (
+    <div>
+      <Box>
+        <MoreVert onClick = {handleClick} />
+        <Menu
+          anchorEl={open}
+          keepMounted
+          open={open}
+          onClose={handleClose}
+          getContentAnchorE1={null}
+        anchorOrigin={{
+            vertical:'bottom',
+            horizontal:'center'
+        }}
+        transformOrigin={{
+            vertical:'top',
+            horizontal:'right'
+        }}
+        >
+          <MenuOption onClick={handleClose}>Profile</MenuOption>
+        </Menu>
+      </Box>
+    </div>
+  );
+};
+
+export default HeaderMenu;
