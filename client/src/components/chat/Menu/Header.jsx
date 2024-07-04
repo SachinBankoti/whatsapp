4f8e647c6ca46cc75115518f312df434b65f1d27
Sachin Bankoti <sachinbankoti16@gmail.com>
2024-07-04 13:01:56 -0400

    added RHS CSS for home whatsapp

--- client/src/components/chat/Menu/Header.jsx
+++ client/src/components/chat/Menu/Header.jsx
@@ -1,15 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AccountContext } from "../../../context/AccountProvider";
-import { Box,styled } from "@mui/material";
-import { Chat as MessageIcon } from '@mui/icons-material';
+import { Box, styled } from "@mui/material";
+import { Chat as MessageIcon } from "@mui/icons-material";
 import HeaderMenu from "./HeaderMenu";
+import InfoDrawer from "../../../drawer/InfoDrawer";
+
 const Component = styled(Box)`
   height: 44px;
   background: #ededed;
   padding: 8px 16px;
   display: flex;
   align-items: center;
-`
+`;
 const Wrapper = styled(Box)`
   margin-left: auto;
   display: flex;
@@ -17,30 +19,33 @@ const Wrapper = styled(Box)`
     margin-left: 2px;
     padding: 8px;
     color: #000;
-  };
-  & : first-child {
+  }
+  &: first-child {
     font-size: 22px;
     margin-right: 8px;
   }
-
-`
-const Image = styled('img')({
+`;
+const Image = styled("img")({
   height: 40,
   width: 40,
-  borderRadius: '50%'
-})
+  borderRadius: "50%",
+});
 const Header = () => {
+  const [openDrawer,setOpenDrawer] = useState(false);
   const { account } = useContext(AccountContext);
+  const toggleDrawer=()=>{
+    setOpenDrawer(true);
+  }
   return (
     <div>
       <Component>
-        <Image src={account.picture} alt="dp" />
+        <Image src={account.picture} alt="dp" onClick={()=> toggleDrawer()}/>
         <Wrapper>
           <MessageIcon />
-          {/* <MoreVert /> */}
-          <HeaderMenu />
+          <HeaderMenu setOpenDrawer={setOpenDrawer}/>
         </Wrapper>
       </Component>
+      <InfoDrawer open={openDrawer} setOpen={setOpenDrawer}/>
     </div>
   );
 };
--- client/src/components/chat/Menu/HeaderMenu.jsx
+++ client/src/components/chat/Menu/HeaderMenu.jsx
@@ -1,40 +1,48 @@
 import { MoreVert } from "@mui/icons-material";
-import { Menu, MenuItem,Box,styled} from "@mui/material";
+import { Menu, MenuItem, Box, styled } from "@mui/material";
 import { useState } from "react";
 
 const MenuOption = styled(MenuItem)`
-    font-size: 14px;
-    padding: 15px 60px 5px 24px;
-    color: #4A4A4A;
-`
-const HeaderMenu = () => {
-    const [open,setOpen] = useState(null);
-    const handleClose =()=>{
-        setOpen(null);
-    }
-    const handleClick =(e)=>{
-        setOpen(e.currentTarget);
-    }
+  font-size: 14px;
+  padding: 15px 60px 5px 24px;
+  color: #4a4a4a;
+  
+`;
+const HeaderMenu = ({ setOpenDrawer }) => {
+  const [open, setOpen] = useState(null);
+  const handleClose = () => {
+    setOpen(null);
+  };
+  const handleClick = (e) => {
+    setOpen(e.currentTarget);
+  };
   return (
     <div>
       <Box>
-        <MoreVert onClick = {handleClick} />
+        <MoreVert onClick={handleClick} />
         <Menu
           anchorEl={open}
           keepMounted
           open={open}
           onClose={handleClose}
           getContentAnchorE1={null}
-        anchorOrigin={{
-            vertical:'bottom',
-            horizontal:'center'
-        }}
-        transformOrigin={{
-            vertical:'top',
-            horizontal:'right'
-        }}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "right",
+          }}
         >
-          <MenuOption onClick={handleClose}>Profile</MenuOption>
+          <MenuOption
+            onClick={() => {
+              handleClose();
+              setOpenDrawer(true);
+            }}
+          >
+            Profile
+          </MenuOption>
         </Menu>
       </Box>
     </div>
